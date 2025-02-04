6213157ab8f47048c4ab6a54535434f5019476b6
Igor Zubkov <igor.zubkov@gmail.com>
2025-02-04 18:53:58 +0100

    Cleanup

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module Errbit
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: ["assets", "tasks"])
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
