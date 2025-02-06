4541a66ead891c7be570a54a12875985dc570fe3
Igor Zubkov <igor.zubkov@gmail.com>
2025-02-06 23:26:01 +0100

    Fix test

--- spec/requests/health_controller_spec.rb
+++ spec/requests/health_controller_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 RSpec.describe HealthController, type: :request do
-  let(:app) { Fabricate(:app, api_key: "APIKEY") }
+  let(:errbit_app) { Fabricate(:app, api_key: "APIKEY") }
 
   describe "api_key_tester" do
     it "will let you know when the api_key is not valid" do
@@ -14,7 +14,7 @@ RSpec.describe HealthController, type: :request do
     end
 
     it "can let you know that the api_key is valid" do
-      get "/health/api-key-tester?api_key=#{app.api_key}"
+      get "/health/api-key-tester?api_key=#{errbit_app.api_key}"
       expect(response).to be_successful
       parsed_response = response.parsed_body
       expect(parsed_response["ok"]).to eq true
