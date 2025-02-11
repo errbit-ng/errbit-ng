a5c0ff6b88121af38e3a5afa26d9832ef885c5fe
Igor Zubkov <igor.zubkov@gmail.com>
2025-02-11 20:13:19 +0100

    Noticed

---
+++ app/models/noticed/application_record.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Noticed
+  class ApplicationRecord
+    include Mongoid::Document
+  end
+end
---
+++ app/models/noticed/notification.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Noticed
+  class Notification < ApplicationRecord
+    # include Rails.application.routes.url_helpers
+    # include Readable
+    # include Translation
+    #
+    # belongs_to :event, counter_cache: true
+    # belongs_to :recipient, polymorphic: true
+    #
+    # scope :newest_first, -> { order(created_at: :desc) }
+    #
+    # delegate :params, :record, to: :event
+  end
+end
