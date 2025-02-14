aa064a6a01c90440cb12bbe95979d5c783919e9f
Igor Zubkov <igor.zubkov@gmail.com>
2025-02-14 20:21:03 +0100

    Update

--- app/models/noticed/event.rb
+++ app/models/noticed/event.rb
@@ -9,6 +9,7 @@ module Noticed
 
     field :type, type: String
     field :params, type: Hash, default: {}
+    field :notifications_count, type: Integer
 
     belongs_to :record, polymorphic: true, optional: true
     has_many :notifications, dependent: :delete_all
