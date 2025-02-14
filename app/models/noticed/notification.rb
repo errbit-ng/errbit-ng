43d3987a2bb4028ecd52e8d87dd4b5effa5eed4b
Igor Zubkov <igor.zubkov@gmail.com>
2025-02-14 20:32:57 +0100

    Update

--- app/models/noticed/notification.rb
+++ app/models/noticed/notification.rb
@@ -6,6 +6,10 @@ module Noticed
     include Readable
     include Translation
 
+    field :type, type: String
+    field :read_at, type: Time
+    field :seen_at, type: Time
+
     belongs_to :event, counter_cache: true
     belongs_to :recipient, polymorphic: true
 
