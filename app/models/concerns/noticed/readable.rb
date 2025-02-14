59cdfc0e4e31e83737c3d2cfcab5156ef64fcd47
Igor Zubkov <igor.zubkov@gmail.com>
2025-02-14 16:22:43 +0100

    Update

--- app/models/concerns/noticed/readable.rb
+++ app/models/concerns/noticed/readable.rb
@@ -11,6 +11,78 @@ module Noticed
       scope :unseen, -> { where(seen_at: nil) }
     end
 
+    class_methods do
+      def mark_as_read_and_seen(**kwargs)
+        update_all(**kwargs.with_defaults(read_at: Time.current, seen_at: Time.current, updated_at: Time.current))
+      end
 
+      def mark_as_unread_and_unseen(**kwargs)
+        update_all(**kwargs.with_defaults(read_at: nil, seen_at: nil, updated_at: Time.current))
+      end
+
+      def mark_as_read(**kwargs)
+        update_all(**kwargs.with_defaults(read_at: Time.current, updated_at: Time.current))
+      end
+
+      def mark_as_unread(**kwargs)
+        update_all(**kwargs.with_defaults(read_at: nil, updated_at: Time.current))
+      end
+
+      def mark_as_seen(**kwargs)
+        update_all(**kwargs.with_defaults(seen_at: Time.current, updated_at: Time.current))
+      end
+
+      def mark_as_unseen(**kwargs)
+        update_all(**kwargs.with_defaults(seen_at: nil, updated_at: Time.current))
+      end
+    end
+
+    def mark_as_read
+      update(read_at: Time.current)
+    end
+
+    def mark_as_read!
+      update!(read_at: Time.current)
+    end
+
+    def mark_as_unread
+      update(read_at: nil)
+    end
+
+    def mark_as_unread!
+      update!(read_at: nil)
+    end
+
+    def mark_as_seen
+      update(seen_at: Time.current)
+    end
+
+    def mark_as_seen!
+      update!(seen_at: Time.current)
+    end
+
+    def mark_as_unseen
+      update(seen_at: nil)
+    end
+
+    def mark_as_unseen!
+      update!(seen_at: nil)
+    end
+
+    def read?
+      read_at?
+    end
+
+    def unread?
+      !read_at?
+    end
+
+    def seen?
+      seen_at?
+    end
+
+    def unseen?
+      !seen_at?
+    end
   end
 end
