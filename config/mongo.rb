2b987a42bdab0691417b8b86b8790c35c24be5a2
Igor Zubkov <igor.zubkov@gmail.com>
2025-02-26 17:26:14 +0100

    Use default rails logger

--- config/mongo.rb
+++ config/mongo.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-log_level = Logger.const_get Errbit::Config.log_level.upcase
-
-Mongoid.logger.level = log_level
-Mongo::Logger.level = log_level
-
 Mongoid.configure do |config|
   uri = if Errbit::Config.mongo_url == "mongodb://localhost"
     "mongodb://localhost/errbit_#{Rails.env}"
